bd569f893ba3398f446514c3bdd8359884091986
Petar <p.todorov212@gmail.com>
2022-08-17 18:02:07 +0300

    wrote tests and finalsied the project

--- migrations/2_deed_migration.js
+++ migrations/2_deed_migration.js
@@ -3,5 +3,5 @@ const one = "0x46bcd4e0e1a7782c9d03577af2f90482f634e420";
 const two = "0x181c5bd230f9f47d50045f16a211b1f06769e106";
 
 module.exports = function (deployer) {
-  deployer.deploy(Deed,one,two,20,{value:100000});
+  deployer.deploy(Deed,one,two,5,{value:100000});
 };
--- test/deedtest.js
+++ test/deedtest.js
@@ -1,16 +1,64 @@
+
+
+
 const Deed = artifacts.require("Deed");
 let deed;
 
 contract ("Deed", (accounts) => {
     beforeEach ( async () => {
         deed= await Deed.deployed();
+
     })
 
     it ( "Only the lawyer can transfer funds in case of premature Death", async () =>{
+        deed = await Deed.new(
+            accounts[0], 
+            accounts[1], 
+            0, 
+            {value: 100000}
+          );
+
 
         const initialBalance =  web3.utils.toBN(await web3.eth.getBalance(accounts[1]))
 
         await deed.suddenDeath({from:accounts[0]})
+
+        const finalBalance = web3.utils.toBN( await web3.eth.getBalance(accounts[1]))
+
+        assert (finalBalance.sub(initialBalance).toNumber()=== 100000)
+        
+    })
+
+    it (" Should not withdraw if too early", async ()=>{
+        deed = await Deed.new(
+            accounts[0], 
+            accounts[1], 
+            3, 
+            {value: 100000}
+          );
+        try{
+        await deed.send_funds();
+        }
+        catch(error){
+            error.message.includes("Funds are still locked");
+            return;
+        }
+        assert(false);
+    })
+
+    it ( "should be able to withdraw funds after the set time has passed", async () =>{
+        deed = await Deed.new(
+            accounts[0], 
+            accounts[1], 
+            0, 
+            {value: 100000}
+          );
+
+        const initialBalance =  web3.utils.toBN(await web3.eth.getBalance(accounts[1]))
+
+        await new Promise (resolve => setTimeout(resolve,5000));
+
+        await deed.send_funds({from:accounts[0]});
 
         const finalBalance = web3.utils.toBN( await web3.eth.getBalance(accounts[1]))
 
